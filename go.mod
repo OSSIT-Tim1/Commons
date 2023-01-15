32b11c9907de704201fde552326292d5067a1744
PetarK <komordzic.sr37.2020@uns.ac.rs>
2023-01-15 02:56:09 +0100

    Added basic files and structs for messages and replys

--- go.mod
+++ go.mod
@@ -3,8 +3,16 @@ module github.com/OSSIT-Tim1/commons
 go 1.19
 
 require (
-	github.com/nats-io/nats.go v1.22.1 // indirect
+	github.com/nats-io/nats.go v1.22.1
+	go.mongodb.org/mongo-driver v1.11.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/nats-io/nats-server/v2 v2.9.11 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20210314154223-e6e6c4f2bb5b // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- saga/messeging/nats/natsPublisher.go
+++ saga/messeging/nats/natsPublisher.go
@@ -1,7 +1,7 @@
 package nats
 
 import (
-	commons "github.com/OSSIT-Tim1/Commons/saga"
+	saga "github.com/OSSIT-Tim1/commons/saga/messeging"
 	"github.com/nats-io/nats.go"
 )
 
@@ -10,5 +10,22 @@ type Publisher struct {
 	subject string
 }
 
-func NewNATSPublisher(host, port, user, password, subject string) (commons., error) {
+func NewNatsPublisher(host, port, user, password, subject string) (saga.Publisher, error) {
+	conn, err := getConnection(host, port, user, password)
+	encConn, err := nats.NewEncodedConn(conn, nats.JSON_ENCODER)
+	if err != nil {
+		return nil, err
+	}
+	return &Publisher{
+		conn:    encConn,
+		subject: subject,
+	}, nil
+}
+
+func (p *Publisher) Publish(message interface{}) error {
+	err := p.conn.Publish(p.subject, message)
+	if err != nil {
+		return err
+	}
+	return nil
 }
